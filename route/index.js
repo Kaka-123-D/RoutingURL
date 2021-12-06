12e287512ff90cede484d249e1f70859672dfd86
Kai <vantienkhai.uet@gmail.com>
2021-12-06 14:39:02 +0700

    routing

--- route/index.js
+++ route/index.js
@@ -1,9 +1,7 @@
 const studentsRouter = require("./students");
-const fallbackRouter = require("./fallback");
 const viewsRouter = require("./views");
 
 module.exports = (app) => {
-  app.use("/views", viewsRouter);
+  app.use("/", viewsRouter);
   app.use("/students", studentsRouter);
-  app.use("/*", fallbackRouter);
 };
--- route/views.js
+++ route/views.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/get|/get.pug", (req, res) => {
+router.get("/get|/get.*", (req, res) => {
   res.render("get")
 });
 
-router.get("/list|/list.pug", (req, res) => {
+router.get("/list|/list.*", (req, res) => {
   res.render("list")
 });
 
-router.get("/update|/update.pug", (req, res) => {
+router.get("/update|/update.*", (req, res) => {
   res.render("update")
+});
+
+//fallback
+router.get("/*", (req, res) => {
+  let ret = {
+    status: "ERR",
+    data: "ACTION-NOT-FOUND",
+  };
+  res.json(ret);
 });
 
 module.exports = router;
