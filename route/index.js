27ed18577dc4745a154ce8092d55c19b4d74c398
Kai <vantienkhai.uet@gmail.com>
2021-12-06 14:49:30 +0700

    last commit

--- route/index.js
+++ route/index.js
@@ -2,6 +2,6 @@ const studentsRouter = require("./students");
 const viewsRouter = require("./views");
 
 module.exports = (app) => {
-  app.use("/", viewsRouter);
   app.use("/students", studentsRouter);
+  app.use("/", viewsRouter);
 };
